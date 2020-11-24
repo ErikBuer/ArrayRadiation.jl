17675e12cbdcce826abc7ff210382b8afd569d1e
ErikBuer <buer94@gmail.com>
2020-11-24 19:21:12 +0100

    pre merge

--- ArrayBeam.jl
+++ ArrayBeam.jl
@@ -1,5 +1,5 @@
 # Copyright (c) 2020 - Norsk Datateknikk AS
-using PyPlot
+using Plots
 include("DspUtility.jl")
 
 # This script calculates the gain of an array atenna with a point source antenna assumption.
@@ -7,7 +7,12 @@ include("DspUtility.jl")
 # Antenna element placement vector
 # Use vector for a linear array
 # Place elements symmetrically around zero
-r = LinRange(-64,64,129)*0.5
+
+# Generate locations (1D) for an λ/2 spaced array of N elements. N is even.
+linearArray(N) = LinRange(-N/2+0.5, N/2-0.5, N)
+
+r = linearArray(4)
+
 # Antenna element weigth
 W = ones(size(r))
 # Frequency of interrest
@@ -20,21 +25,25 @@ c = 3e8
 Ge_dB = 0   # isotropic
 Ge = db2pow(Ge_dB)
 
-# Array gain at angle k [rad]
+# Array normal angle k [rad]
 k0	= 0
 
+# Array gain at angle k [rad]
 function GΩ_dB(k, k0, Ge)
 	Σ = sum
 	i = im
 	numerator(m)= W[m]*exp(-i*(k-k0)*r[m])
 	vecIndex 	= trunc.(Int64, LinRange( 1, size(r)[1], size(r)[1]))
-	return Ge*broadcast( abs, Σ( numerator, vecIndex ) )^2 / Σ(Σ(broadcast(abs, W).^2)) 
+	return Ge*abs.( Σ( numerator, vecIndex ) )^2 / Σ(abs.(W).^2) 
 end
+
+# TODO account for forshortening
+# TODO account for cosine tapering
 
 gain(k) = GΩ_dB(k, k0, Ge)
 
-angleRad 		= LinRange( -π, π, 513)
-angledeg			= rad2deg.(angleRad)
+angleRad 		= LinRange( -π, π, 1001)
+angledeg		= rad2deg.(angleRad)
 radPattern		= broadcast(gain, angleRad)
 radPattern_dB	= broadcast( pow2db, radPattern )
 
