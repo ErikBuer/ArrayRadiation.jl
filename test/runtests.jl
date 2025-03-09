31b50c7dd862d8d65a6cd356af9ebb4de548e3a3
Erik Buer <buer94@gmail.com>
2025-03-09 10:56:53 +0100

    Limit tests used
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,7 @@
 using Test
-
-#push!(LOAD_PATH, expanduser(".")) # Assumed to be ran from the repl folder.
-
-#include("ArrayRadiation.jl")
 using ArrayRadiation
 
 
-include("doctest.jl")
-include("test_gain_calculation.jl")
+#include("doctest.jl")
+#include("test_gain_calculation.jl")
+#include("plot_window_functions.jl")
