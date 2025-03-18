2080059b671e80113d076df38afc8b1e999a5c95
Erik Buer <buer94@gmail.com>
2025-03-18 19:49:44 +0100

    Remove unused submodule
    Signed-off-by: Erik Buer <buer94@gmail.com>

--- src/ArrayRadiation.jl
+++ src/ArrayRadiation.jl
@@ -6,15 +6,12 @@ using .DspUtility
 include("Window.jl")
 using .Window
 
-include("FrequencyDomain.jl")
-using .FrequencyDomain
-
 include("Kspace.jl")
 using .Kspace
 
 include("AntennaElement.jl")
 using .AntennaElement
 
-export ArrayTools, DspUtility, Kspace, FrequencyDomain, Window, AntennaElement
+export ArrayTools, DspUtility, Kspace, Window, AntennaElement
 
 end
